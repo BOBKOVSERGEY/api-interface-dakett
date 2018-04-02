486abf4d8828d8bbd24add9cb7b97d8205a804ad
SERGEYBOBKOV <sergey_bobkov@inbox.ru>
2018-04-02 10:41:07 +0300

    angular

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var bs = require("browser-sync").create();
 
-bs.watch(['history/*.*', 'history/css/*.*']).on("change", bs.reload);
+bs.watch(['angular/*.*', 'history/css/*.*']).on("change", bs.reload);
 
 // Start a Browsersync proxy
 bs.init({
-  server: "./history"
+  server: "./angular"
 });
